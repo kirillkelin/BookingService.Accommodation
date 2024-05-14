51dc1c59bbbc45295aaa887a4079ece7f5963ba1
kirillkelin <kelinkirill@gmail.com>
2024-05-14 13:32:43 +0300

    dev 3 feat (Сервис booking)

---
+++ app/Services/BookingService.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Accommodation;
+use App\Repositories\AccommodationRepository;
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class BookingService
+{
+    public function __construct(private readonly AccommodationRepository $accommodationRepository)
+    {
+    }
+
+    public function getBookingsByLandlordId(Request $request): string
+    {
+        $landlordId = $request->input('landlord_id');
+        $accommodationId = $this->getAccommodationByLandlord($landlordId)->id;
+
+        $client = new Client(['base_uri' => 'http://booking-service.loc']);
+        $response = $client->get('api/bookings/get', [
+            'query' => ['accommodation_id' => $accommodationId],
+        ]);
+
+        return $response->getBody();
+    }
+
+    private function getAccommodationByLandlord(int $id): Accommodation
+    {
+        return $this->accommodationRepository->getByLandlordId($id);
+    }
+
+}
